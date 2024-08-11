bbe087a60e2469ba9fab3b83a77e0a75a29b3427
Muhammad Abdullah Mahmood <muhammadabdullahmahmood@gmail.com>
2024-08-12 00:41:31 +0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,73 +1,11 @@
-# import requests
-# import json
-
-# output_storage=[{}]
-
-# def query_llama(prompt):
-#     url = "https://openrouter.ai/api/v1/chat/completions"
-#     headers = {
-#         "Authorization": "Bearer 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",
-
-#         "Content-Type": "application/json"
-#     }
-#     data = {
-#         "model": "meta-llama/llama-3.1-8b-instruct:free",
-#         "messages": [
-            
-#             {"role": "system", "content":f"you are a personal communication AI coach named AmplifyAI and your goal is to make everyone the get the best version out of themselves.this is the conversation's history about user's prompt and your response {output_storage}.If anyone tends to take on the conversation to the wrong side, deal with them delightly."},
-
-#             {"role": "user", "content": prompt}
-#         ]
-#     }
-
-#     try:
-#         response = requests.post(url, headers=headers, json=data)
-#         response.raise_for_status()  # Raises an HTTPError for bad responses 
-        
-#         result = response.json() # response decoder with json()
-
-#         if 'choices' in result and len(result['choices']) > 0:
-#            #prompt_history.append(result['choices'][0]['message']['content'])
-#             return result['choices'][0]['message']['content']
-#         else:
-#             return "No response generated."
-    
-#     except requests.exceptions.RequestException as e:
-#         return f"An error occurred: {str(e)}"
-
-
-# inp_prompt= ""
-# prompt =""
-
-
-# while prompt != "end":
-#     prompt=input("enter your message :")
-#     inp_prompt=prompt
-#     response = query_llama(inp_prompt)
-#     print(f"prompt: {prompt}")
-#     print(f"response: {response}")
-#     output_storage.append({"user":prompt,"output":response})
-
-
-
-
-
-
-
-
-
-
-
-
-
 import streamlit as st
 import requests
 import json
 
 def query_llama(prompt, output_storage):
-    url = "https://openrouter.ai/api/v1/chat/completions"
+    url = "open-router-link"
     headers = {
-        "Authorization": "Bearer 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",
+        "Authorization": "Bearer api-key",
         "Content-Type": "application/json"
     }
     data = {
